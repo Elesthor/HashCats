39a966e08987241814338a7e089d97c9de35ccbc
Elesthor <elesthor@gmail.com>
2014-04-05 09:07:52 +0200

    Add Input

---
+++ Inputs/list.py
@@ -0,0 +1,13 @@
+import sys
+
+
+def labyrinthe(N, M, tab):
+    # Inserez votre code ici
+    return
+
+
+if __name__ == '__main__':
+    N = int(raw_input())
+    M = int(raw_input())
+    tab = [[int(_) for _ in raw_input().split()] for i in xrange(N)]
+
---
+++ Inputs/map.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def tower_defense(rows, cols, theMap):
+    # Inserez votre code ici
+    return True
+
+
+if __name__ == '__main__':
+    rows = int(raw_input())
+    cols = int(raw_input())
+    theMap = [list(raw_input()) for i in xrange(rows)]
