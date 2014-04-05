cfd4cf35939376773125aab14081046a2e1652db
Saroupille <franth2@gmail.com>
2014-04-05 11:41:03 +0200

    input

--- Inputs/test.py
+++ Inputs/test.py
@@ -1,14 +1,23 @@
 def readFile(path):
     f=open(path,"r")
-    (nintersections,streets, time, cars, start )=map(int, f.readline().split())
+    (nintersections,nstreets, time, cars, start )=map(int, f.readline().split())
     print(nintersections)
-    print(streets)
+    print(nstreets)
     print(time)
     print(cars)
     print(start)
     intersection={(i,tuple((map(float, f.readline().split())))) for i in range(nintersections)}
-    print(intersection)
-                  
+    #print(intersection)
+    #streets={(i, tuple(map(int, f.readline().split()))) for i in range(nstreets)}   
+    #print(streets)
+    streets=[tuple() for i in range(nintersections) for j in range(nintersections)]
+    for line in f.readlines():
+        
+        a=list(map(int, line.split()))
+        print(a)
+        streets2[a[0]][a[1]]=tuple(a)
+    print(streets2)
+        
 #def writeFile:
     
 
